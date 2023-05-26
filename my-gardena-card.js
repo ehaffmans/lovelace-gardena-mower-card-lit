f3c6660c285d362b61827168b6cb731ea82a3d0b
ehaffmans <42911247+ehaffmans@users.noreply.github.com>
2023-05-26 20:29:27 +0200

    Update my-gardena-card.js
    Crude lit implementation of my-gardena-card

--- my-gardena-card.js
+++ my-gardena-card.js
@@ -1,14 +1,18 @@
-class MyGardenaCard extends Polymer.Element {
+import {
+  LitElement,
+  html,
+  css,
+} from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
 
-    static get template() {
-        return Polymer.html`
-          <style>
+class MyGardenaCard extends LitElement {
+  static styles = css`
             .background {
               background-repeat: no-repeat;
               background-position: center center;
               background-size: cover;
             }
             .title {
+			        display:block!important;
               font-size: 20px;
               padding: 16px 16px 0;
               text-align: center;
@@ -22,7 +26,7 @@ class MyGardenaCard extends Polymer.Element {
             .flex {
               display: flex;
               align-items: center;
-              justify-content: space-evenly;
+              justify-content: space-between;
             }
             .button {
               cursor: pointer;
@@ -46,99 +50,147 @@ class MyGardenaCard extends Polymer.Element {
             }
             .grid-right {
               text-align: right;
+			  font-size: 110%;
               padding-right: 10px;
               border-right: 2px solid var(--primary-color);
             }
-          </style>
-          <ha-card hass="[[_hass]]" config="[[_config]]" class="background" style="[[background]]">
-            <template is="dom-if" if="{{name}}">
-              <div class="title" style="[[text]]">[[name]]</div>
-            </template>
-            <div class="content" on-click="moreInfo" style="[[padding]]">
-              <div class="grid" style="[[text]]">
-                <div class="grid-content grid-left">
-                  <div>Status: [[stateObj.attributes.state]]</div>
-                  <div>Battery: [[stateObj.attributes.battery_level]] %</div>
-                  <div>Radio: [[stateObj.attributes.rf_link_level]] %</div>
-                </div>
-                <template is="dom-if" if="{{showDetails}}">
-                  <div class="grid-content grid-right" >
-                    <div>Last Error: [[stateObj.attributes.last_error]]</div>
-                    <div>Battery State: [[stateObj.attributes.battery_state]] </div>
-                    <div>Cutting Time: [[stateObj.attributes.operating_hours]] h</div>
-                  </div>
-                </template>
-              </div>
+  `;
+
+  static properties = {
+    _hass: {},
+    _config: {},
+    name: {},
+    stateObj: {},
+    buttons: {},
+    padding: {},
+    background: {},
+    text: {},
+    vendor: {},
+    showDetails: {},
+  };
+
+  render() {
+    return html`
+      <ha-card
+        .hass=${this._hass}
+        .config=${this._config}
+        class="background"
+        style=${this.background}
+      >
+        ${this.name
+          ? html`
+              <div class="title" style=${this.text}>${this.name}</div>
+            `
+          : ''}
+        <div class="content" @click=${this.moreInfo} style=${this.padding}>
+          <div class="grid" style=${this.text}>
+            <div class="grid-content grid-left">
+              <div>Status: ${this.stateObj.attributes.state}</div>
+              <div>Battery: ${this.stateObj.attributes.battery_level} %</div>
+              <div>Radio: ${this.stateObj.attributes.rf_link_level} %</div>
             </div>
-            <template is="dom-if" if="{{buttons}}">
+            ${this.showDetails
+              ? html`
+                  <div class="grid-content grid-right">
+                    <div>Last Error: ${this.stateObj.attributes.last_error}</div>
+                    <div>Battery State: ${this.stateObj.attributes.battery_state}</div>
+                    <div>Cutting Time: ${this.stateObj.attributes.operating_hours} h</div>
+                  </div>
+                `
+              : ''}
+          </div>
+        </div>
+        ${this.buttons
+          ? html`
               <div class="flex">
-                <div class="button" on-tap="startMower">
+                <div class="button" @click=${this.startMower}>
                   <ha-icon icon="mdi:play"></ha-icon>
                 </div>
-                <div class="button" on-tap="stopMower">
+                <div class="button" @click=${this.stopMower}>
                   <ha-icon icon="mdi:stop"></ha-icon>
                 </div>
-                <div class="button" on-tap="returnMower">
+                <div class="button" @click=${this.returnMower}>
                   <ha-icon icon="mdi:home-map-marker"></ha-icon>
                 </div>
               </div>
-            </template>
-          </ha-card>
-        `;
+            `
+          : ''}
+      </ha-card>
+    `;
+  }
+
+  moreInfo() {
+    this.dispatchEvent(new CustomEvent('hass-more-info', { bubbles: true, composed: true }));
+  }
+
+  startMower() {
+    this.callService(this.vendor === 'ecovacs' ? 'turn_on' : 'start');
+  }
+
+  stopMower() {
+    this.callService(this.vendor === 'ecovacs' ? 'turn_off' : 'stop');
+  }
+
+  returnMower() {
+    this.callService('return_to_base');
+  }
+
+  callService(service) {
+    this._hass.callService('vacuum', service, { entity_id: this._config.entity });
+  }
+
+  fireEvent(type, options = {}) {
+    const event = new CustomEvent(type, {
+      bubbles: options.bubbles || true,
+      cancelable: options.cancelable || true,
+      composed: options.composed || true,
+      detail: { entityId: this._config.entity },
+    });
+    this.shadowRoot.dispatchEvent(event);
+    return event;
+  }
+
+  getCardSize() {
+    if (this.name && this.buttons) return 5;
+    if (this.name || this.buttons) return 4;
+    return 3;
+  }
+
+  setConfig(config) {
+    if (!config.entity) throw new Error('Please define an entity.');
+    if (config.entity.split('.')[0] !== 'vacuum')
+      throw new Error('Please define a vacuum entity.');
+
+    this.buttons = config.buttons !== false;
+    this.padding = `padding: ${this.buttons ? '16px 16px 4px' : '16px'}`;
+    this.background = config.background !== false
+      ? `background-image: url('/local/${config.background || 'img/mower.png'}')`
+      : '';
+    this.text = `color: ${config.background !== false
+      ? 'white; text-shadow: 0 0 10px black;'
+      : 'var(--primary-text-color)'}`;
+
+    this.vendor = config.vendor || 'gardena';
+    this.showDetails = this.vendor !== 'ecovacs';
+
+    this._config = config;
+  }
+
+  set hass(hass) {
+    this._hass = hass;
+
+    if (hass && this._config) {
+      this.stateObj = this._config.entity in hass.states
+        ? hass.states[this._config.entity]
+        : null;
+
+      if (this.stateObj) {
+        this.name =
+          this._config.name !== false &&
+          (this._config.name || this.stateObj.attributes.friendly_name);
+      }
     }
-
-    moreInfo() { this.fireEvent('hass-more-info'); }
-    startMower() { this.callService(this.vendor === 'ecovacs' ? 'turn_on' : 'start'); }
-    stopMower() { this.callService(this.vendor === 'ecovacs' ? 'turn_off' : 'stop'); }
-    returnMower() { this.callService('return_to_base'); }
-
-    callService(service) {
-        this._hass.callService('vacuum', service, {entity_id: this._config.entity});
-    }
-
-    fireEvent(type, options = {}) {
-        const event = new Event(type, {
-            bubbles: options.bubbles || true,
-            cancelable: options.cancelable || true,
-            composed: options.composed || true,
-        });
-        event.detail = {entityId: this._config.entity};
-        this.shadowRoot.dispatchEvent(event);
-        return event;
-    }
-
-    getCardSize() {
-        if (this.name && this.buttons) return 5;
-        if (this.name || this.buttons) return 4;
-        return 3;
-    }
-
-    setConfig(config) {
-        if (!config.entity) throw new Error('Please define an entity.');
-        if (config.entity.split('.')[0] !== 'vacuum') throw new Error('Please define a vacuum entity.');
-
-        this.buttons = config.buttons !== false;
-        this.padding = `padding: ${this.buttons ? '16px 16px 4px' : '16px'}`;
-        this.background = config.background !== false ? `background-image: url('/local/${config.background || 'img/mower.png'}')` : '';
-        this.text = `color: ${config.background !== false ? 'white; text-shadow: 0 0 10px black;' : 'var(--primary-text-color)'}`;
-
-        this.vendor = config.vendor || 'gardena';
-        this.showDetails = this.vendor !== 'ecovacs';
-
-        this._config = config;
-    }
-
-    set hass(hass) {
-        this._hass = hass;
-
-        if (hass && this._config) {
-            this.stateObj = this._config.entity in hass.states ? hass.states[this._config.entity] : null;
-
-            if (this.stateObj) {
-                this.name = this._config.name !== false && (this._config.name || this.stateObj.attributes.friendly_name);
-            }
-        }
-    }
+  }
 }
 
 customElements.define('my-gardena-card', MyGardenaCard);
